68aafad667f85cd55fb270360fac6fff3714452c
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-19 02:34:51 +0700

    .

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ import express from "express";
 import FileUpload from "express-fileupload";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import ProductRoute from "./routes/ProductRoute.js";
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use(FileUpload());
+app.use(ProductRoute);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
