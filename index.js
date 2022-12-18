b808bb28589a5d3de7aadfc7b0d33839b8a27c04
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-18 23:10:44 +0700

    Initial setup

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import FileUpload from "express-fileupload";
+import cors from "cors";
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(FileUpload());
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
