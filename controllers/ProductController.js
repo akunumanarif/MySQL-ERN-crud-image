85b68150b2c53d9c02d5a9a1d6a6ce315dc390bf
M Nu'man Arif H <numanarif87@gmail.com>
2022-12-19 04:58:05 +0700

    .

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,7 +1,25 @@
 import Product from "../models/ProductModel.js";
 
-export const getProduct = (req, res) => {};
-export const getProductById = (req, res) => {};
+export const getProduct = async (req, res) => {
+  try {
+    const response = await Product.findAll();
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
+export const getProductById = async (req, res) => {
+  try {
+    const response = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json(error);
+  }
+};
 export const saveProduct = (req, res) => {};
 export const updateProduct = (req, res) => {};
 export const deleteProduct = (req, res) => {};
